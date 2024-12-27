8611b518917fa0253767a000126f94710db1ba9d
NarendraTezu <narendra.tezu18@gmail.com>
2024-12-27 23:18:40 +0530

    binary search to serach a number in array:wq

--- binary_search.py
+++ binary_search.py
@@ -1,9 +1,22 @@
 arr = [11,12,14,16,17,19,22,25,28,29,43,46,49,50]
 
-def binary_serch(arr,x):
+def binary_search(arr,x):
     start = 0
-    end = len(arr)
-    mid = start + end // 2
+    end = len(arr) - 1
 
-    while(start < end):
+    while(start <= end):
+        mid = (start + end) // 2
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] < x:
+            start = mid + 1
+        else:
+            end = mid - 1
+
+    return -1
+
+
+res = binary_search(arr, 43)
+print(f"Number is present at the index : ", res)
+        
 
