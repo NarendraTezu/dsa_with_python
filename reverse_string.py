8f7bd6252efbdd79d0499d5d3eaeaa181f2fb999
NarendraTezu <narendra.tezu18@gmail.com>
2025-09-15 10:31:59 +0530

    this program will reverse string

---
+++ reverse_string.py
@@ -0,0 +1,16 @@
+# function defination
+
+def reverse(arr):
+    reverse_arr = arr[::-1]
+    return reverse_arr
+
+
+#input 
+
+arr = [1,4,6,8,11]
+
+# function call
+
+result = reverse(arr)
+
+print(result)
